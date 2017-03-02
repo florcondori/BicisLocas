f85b4a20542e192936208a73c226eb5c80b76e87
flor condori <condorihuamanif@gmail.com>
2017-03-02 18:30:32 -0500

    campos obligatorios

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
 function validateForm(){
-	/* Escribe tú código aquí */
+	var nombre = document.getElementById('name').value;
+	var apellido = document.getElementById('lastname').value;
+	var email = document.getElementById('input-email').value;
+	var password = document.getElementById('input-password').value;
+	var tipo_bici = document.getElementById('tipo-bici').value;
+	
+
+	if(nombre =="" || apellido =="" || email =="" || password ==""){
+		alert("llenar campos obliggatorios");
+	}
+	
+
 }
